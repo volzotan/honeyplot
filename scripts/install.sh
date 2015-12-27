9c730c3dce9fce84f4f86b948b7c1b9c854d37f6
Christopher Getschmann <christopher@volzo.de>
2015-12-27 13:35:56 +0100

    upload script for controller

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 # sudo apt-get update
 # sudo apt-get upgrade
 
-DEPENDENCIES="avrdude picocom"
+DEPENDENCIES="avrdude picocom python-pip"
 
 sudo apt-get install $DEPENDENCIES
+
+mkdir -p controller/firmware
